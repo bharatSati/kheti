7e8425265903561bd127ee38cd36b065a07b442d
Bharat sati <mailforcodingg@gmail.com>
2025-09-27 01:45:38 +0530

    debug7

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://kheti.vercel.app/",
+  origin: "https://kheti.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
